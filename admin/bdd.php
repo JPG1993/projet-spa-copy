0e1aaabebf145de69d5a2f7b1befe6f4fe16147f
JPG1993 <jpjosephgarboud@gmail.com>
2022-01-19 11:38:47 +0100

    modif bdd admin

--- admin/bdd.php
+++ admin/bdd.php
@@ -1,9 +1,10 @@
 <?php
 $bdd = new PDO('mysql:host=localhost;dbname=adoption_chat', "root", "");
 
-    // if($bdd){
-    //     echo "connection successfull";
-    // }else{
-    //     die(mysqli_error($bdd));
-    // }
+    if($bdd){
+        echo "connection successfull";
+    }
+    else{
+        var_dump($bdd);
+    }
 ?>

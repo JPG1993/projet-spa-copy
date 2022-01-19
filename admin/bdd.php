f200ef630fb04dea3e90fa8aff7a673f9a58ca93
JPG1993 <jpjosephgarboud@gmail.com>
2022-01-19 11:08:13 +0100

    modifprojet

---
+++ admin/bdd.php
@@ -0,0 +1,9 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=adoption_chat', "root", "");
+
+    // if($bdd){
+    //     echo "connection successfull";
+    // }else{
+    //     die(mysqli_error($bdd));
+    // }
+?>
